fa7539e2921e4e47e32dc99b4355fe05e55c55b0
Philippe MEYER <p.meyer@mappingcontrol.com>
2018-10-05 14:21:54 +0200

    added controls to allow designing, playing, saving erasing curves

--- spiralsAndCurves.js
+++ spiralsAndCurves.js
@@ -7,98 +7,102 @@ DATE : 2018-10-02
 var globals = {
 	mainHeight : 640,
 	mainWidth : 640,
-	baseW : 640,
-	baseH: 640, 
 	mode : "P2D",
 	baseUnit:100,
 	strokeWeight :1,
 	strokeColor :  {r:0,g:0,b:0},
-	backgroundColor:{r:225,g:225,b:225}
-
+	backgroundColor:{r:255,g:255,b:255},
+    curveType : "curve",
+	saves : [],
+	state:"design"
 };
 var CrtlPt1,CrtlPt2,ptA,ptB;
 var arrObjects = [];
+
 function setup() {
     var mode = (globals.mode == "WEBGL") ? WEBGL : P2D;
 	var cnv = createCanvas(globals.mainWidth,globals.mainHeight,mode);
     cnv.parent('canvasZone');
-	ptA = {x:globals.mainWidth/2,y:100,isDragged:false,radius:10,name:"A",color:"#00ff00"};
-	ptB = {x:globals.mainWidth/3,y:globals.mainHeight-100,isDragged:false,radius:10,name:"B",color:"#ee0000"};
 	
-	//line(ptA.x,ptA.y,ptB.x,ptB.y);
+	document.getElementById(globals.curveType).checked = true;
 	
-	CrtlPt1 = {x:100,y:80,isDragged:false,radius:10,name:"1",color:"#0000ff"};
-	CrtlPt2 = {x:200,y:160,isDragged:false,radius:10,name:"2",color:"#0000ff"};
+	updateUI();
+
+	setListeners();
+	
+	ptA = {x:round(globals.mainWidth/2),y:100,isDragged:false,radius:10,name:"A",color:"#00ff00",shape:"rect"};
+	ptB = {x:round(globals.mainWidth/3),y:globals.mainHeight-100,isDragged:false,radius:10,name:"B",color:"#ee0000",shape:"rect"};
+		
+	CrtlPt1 = {x:100,y:80,isDragged:false,radius:10,name:"1",color:"#0000ff",shape:"ellipse"};
+	CrtlPt2 = {x:200,y:160,isDragged:false,radius:10,name:"2",color:"#0000ff",shape:"ellipse"};
 	arrObjects = [CrtlPt1,CrtlPt2,ptA,ptB];
 }
 
-function spiral(startAngle,startPoint,angleOffset,nrPoints,a,b,sign,startFct){
-	var x,y;
+function updateUI(){
+	var toggler = document.getElementById("toggleState");
+	if(globals.state == "design"){
+		toggler.value = "Play";
+	}else{
+		toggler.value = "Design";
+	}
+}
+
+function setFormulasInUI(){
+	var html = "";
+	if(globals.saves.length >0){
+		globals.saves.forEach(function(x){
+			html+=x;
+		});
+		html=html.replace(/[;]/g, ';<br/>');
+	}
+	document.getElementById("formulas").innerHTML=html;
+}
+
+function setListeners(){
+	var curveElements = document.getElementsByName("curveType");
+	curveElements.forEach(function(x){
+			x.addEventListener("click",function(){
+			globals.curveType = this.value;
+		},false);
+	});
 	
-	sign = sign || 1;
-	if(startFct) startFct();
-	push();
-	translate(startPoint.x, startPoint.y);
-
-	var angle = startAngle;
-	for(var i = 0; i <nrPoints;i++){
-		if(i<50) {stroke(255,0,0) } else {stroke(0)}
-		r = a*Math.pow(b,angle);
-		x = sign*cos(angle)*r;
-		y = sign*sin(angle)*r;
-		point(x, y);
-		angle += angleOffset;
-	}
-
-	pop();
-}
-
-function spiralOfLength(startAngle,startPoint,angleOffset,maxLengthInLine,a,b,sign,startFct){
-	var x,y,prevX,prevY;
-	var lastSpiralPoint = {x:0,y:0};
-	sign = sign || 1;
-	if(startFct) startFct();
-	push();
-	translate(startPoint.x, startPoint.y);
-    var nrPoints = 10000;
-	var angle = startAngle;
-	var prevX = 0;
-	r = a*Math.pow(b,angle);
-	prevX = sign*cos(angle)*r;
-	prevY = sign*sin(angle)*r;
-	for(var i = 1; i < nrPoints;i++){
-		r = a*Math.pow(b,angle);
-		x = sign*cos(angle)*r;
-		y = sign*sin(angle)*r;
-		line(prevX,prevY,x,y);
-		prevX = x;
-		prevY = y;
-		angle += angleOffset;
-		var len = getLineLength(startPoint.x,startPoint.y,x,y);
-		if(len >= maxLengthInLine) {
-			i = nrPoints; // get out of the loop
-			lastSpiralPoint.x = x;
-			lastSpiralPoint.y= y;
+	document.getElementById("save").addEventListener("click",function(){
+		var formula = null;
+		if(globals.curveType=="bezier"){
+			formula = "bezier("+ptA.x+","+ptA.y+","+CrtlPt1.x+","+CrtlPt1.y+","+CrtlPt2.x+","+CrtlPt2.y+","+ptB.x+","+ptB.y+");"
+			
+		}
+		if(globals.curveType=="curve"){
+			formula = "curve("+CrtlPt1.x+","+CrtlPt1.y+","+ptA.x+","+ptA.y+","+ptB.x+","+ptB.y+","+CrtlPt2.x+","+CrtlPt2.y+");"
+		}
+		
+		if(formula!=null){
+			if(globals.saves.indexOf(formula)==-1){
+				globals.saves.push(formula);
+				setFormulasInUI();
 			}
-	}
-
-	pop();
-	return lastSpiralPoint;
-}
-
-function getLineLength(x1,y1,x2,y2){
-	var diffX = x2 - x1;
-	var diffY = y2 - y1;
-	return sqrt(diffX*diffX + diffY*diffY);
-}
-
-function getStokeWeight(){
-	var strokeWht = globals.strokeWeight;
-	if (globals.baseUnit  < 100){
-		strokeWht = round(globals.baseUnit  / 20);
-		if(strokeWht < 1) strokeWht = 1;
-	}
-	return strokeWht;
+		}
+		
+	},false);
+		
+	document.getElementById("toggleState").addEventListener("click",function(){
+			globals.state = globals.state == "design" ? "play" : "design";
+			updateUI();
+		},false);
+		
+	document.getElementById("eraseLast").addEventListener("click",function(){
+			if(globals.saves.length >0){
+				globals.saves.pop();
+				setFormulasInUI();
+			}
+		},false);
+		
+	document.getElementById("eraseAll").addEventListener("click",function(){
+			if(globals.saves.length >0){
+				globals.saves.length = 0;
+				setFormulasInUI();
+			}
+		},false);
 }
 
 function draw() {
@@ -106,56 +110,67 @@ function draw() {
 	stroke(globals.strokeColor.r,globals.strokeColor.g,globals.strokeColor.b);
 	noFill();
 	strokeWeight(getStokeWeight());
-	var sizeMultiplier = globals.baseUnit / 100;
-	var w = globals.baseW * sizeMultiplier / 2;
-	var h = globals.baseH * sizeMultiplier / 4;
 
-	var top = h;
-	var delta = w / 2;
-	
-	var deltaTop = top + delta;
-	var delta2 = delta / 2;
-	var delta4 = delta / 4;
-	var delta6= delta / 6;
-	var delta8 = delta / 8;
-	var delta12 = delta / 12;
-	
+	if( globals.state == "design" ){
+		if(globals.curveType=="bezier"){
+			push();
+				stroke(0,0,0);
+				bezier(ptA.x,ptA.y,CrtlPt1.x,CrtlPt1.y,CrtlPt2.x,CrtlPt2.y,ptB.x,ptB.y);
+			pop();
+		}
+		if(globals.curveType=="curve"){
+			push();
+				stroke(0,0,0);
+				curve(CrtlPt1.x,CrtlPt1.y,ptA.x,ptA.y,ptB.x,ptB.y,CrtlPt2.x,CrtlPt2.y);
+			pop();
+		}
 
-	
-	
-	push()
-		stroke(0,255,0);
-		bezier(ptA.x,ptA.y,CrtlPt1.x,CrtlPt1.y,CrtlPt2.x,CrtlPt2.y,ptB.x,ptB.y);
-	push()
-	stroke(255,0,0);
-		curve(CrtlPt1.x,CrtlPt1.y,ptA.x,ptA.y,ptB.x,ptB.y,CrtlPt2.x,CrtlPt2.y);
-	pop();
-
-	arrObjects.forEach(function(pt){
-		drawObject(pt);
-	});
-	push()
-		textSize(12);
-		stroke(0);
-		fill(0);
-		text("Bezier in green and curve in red",20,globals.mainHeight-30);
-	pop();
+		push();
+			noFill();
+			stroke("#999");
+			playCurves()
+		pop();
 		
+		arrObjects.forEach(function(pt){
+			drawObject(pt);
+		});
+	}else{
+		playCurves()
+
+	}
 }
 
-
+function playCurves(){
+	var instructions = "";
+	if(globals.saves.length >0){
+		instructions = globals.saves.join(" ");
+	var fct= new Function (instructions);
+	fct();
+	}
+}
 
 function drawObject(pt){
 	push()
+		rectMode(CENTER)
 		ellipseMode(CENTER);
 		strokeWeight(1);
 		fill(pt.color);
 		stroke(pt.color);
-		ellipse(pt.x, pt.y, pt.radius*2, pt.radius*2); 
-		textSize(16);
-		stroke(255);
-		fill(255);
-		text(pt.name,pt.x-pt.radius/2, pt.y+pt.radius/2);
+		if(pt.isDragged){
+			stroke(0);
+			strokeWeight(3);
+			point(pt.x, pt.y);
+		}else if(pt.shape=="ellipse"){
+			ellipse(pt.x, pt.y, pt.radius*2, pt.radius*2); 
+		}else if(pt.shape=="rect"){
+			rect(pt.x, pt.y, pt.radius*2, pt.radius*2); 
+		}
+		if(!pt.isDragged){
+			textSize(16);
+			stroke(255);
+			fill(255);
+			text(pt.name,pt.x-pt.radius/2, pt.y+pt.radius/2);
+		}
 		textSize(12);
 		stroke(0);
 		fill(0);
@@ -172,18 +187,20 @@ function mouseDragged() {
 			 pt.y = y;
 		}
 	});
-  // prevent default
   return false;
 }
 
 function mousePressed() {
   var x = mouseX;
   var y = mouseY;
-
+  var oneFound = false;
   arrObjects.forEach(function(pt){
-  if(x <= pt.x+pt.radius && x >= pt.x-pt.radius && y <= pt.y+pt.radius && y >= pt.y-pt.radius ){
-		pt.isDragged = true;
+	  if(!oneFound){
+		if(x <= pt.x+pt.radius && x >= pt.x-pt.radius && y <= pt.y+pt.radius && y >= pt.y-pt.radius ){
+			pt.isDragged = true;
+			oneFound = true;
 		}
+	  }
 	});
 }
 
@@ -192,12 +209,40 @@ function mouseReleased() {
   var x = mouseX;
   var y = mouseY;
   
+
+  
    arrObjects.forEach(function(pt){
 	if(pt.isDragged ){
-		 pt.x = x;
-		 pt.y = y;
+		pt = stayInside(pt,x,y);
 	 }
 	 pt.isDragged = false;
    });
   
+}
+
+function stayInside(pt,x,y){
+ if(x < 0){
+	  x = pt.radius;
+  }
+    if(y < 0){
+	  y = pt.radius;
+  }
+    if(x > globals.mainWidth){
+	  x = globals.mainWidth - pt.radius;
+  }
+    if(y > globals.mainHeight){
+	  y = globals.mainHeight - pt.radius;
+  }
+  pt.x = round(x);
+  pt.y = round(y);
+  return pt;
+}
+
+function getStokeWeight(){
+	var strokeWht = globals.strokeWeight;
+	if (globals.baseUnit  < 100){
+		strokeWht = round(globals.baseUnit  / 20);
+		if(strokeWht < 1) strokeWht = 1;
+	}
+	return strokeWht;
 }
